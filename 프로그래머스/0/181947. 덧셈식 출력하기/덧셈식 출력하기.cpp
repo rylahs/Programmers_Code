1c13f434d3e63727038eb469aec4fe14c83cbe75
Rylah <94848571+rylahs@users.noreply.github.com>
2026-05-27 03:58:02 +0900

    [level 0] Title: 덧셈식 출력하기, Time: 1.84 ms, Memory: 4.07 MB -BaekjoonHub

---
+++ 프로그래머스/0/181947. 덧셈식 출력하기/덧셈식 출력하기.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int a, b;
+    cin >> a >> b;
+    
+    cout << a << " + " << b << " = " << a + b << '\n';
+    return 0;
+}
