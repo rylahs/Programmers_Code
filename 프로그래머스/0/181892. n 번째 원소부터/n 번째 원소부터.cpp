8ab9f6bb90b5c63aa54e6674080155303c5fdcc4
Rylah <94848571+rylahs@users.noreply.github.com>
2026-06-08 20:25:18 +0900

    [level 0] Title: n 번째 원소부터, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list, int n) {
+    vector<int> answer;
+    answer.reserve(num_list.size() - n + 1);
+    for (int i = n - 1; i < num_list.size(); i++)
+        answer.emplace_back(num_list[i]);
+    return answer;
+}
